ac4ca8b7e0cbf572d26adb40bc6b3371c7341a3c
wolf1729 <aryanm1729@gmail.com>
2024-07-03 16:09:03 +0530

    new changes

--- src/chapters/footer.tsx
+++ src/chapters/footer.tsx
@@ -9,11 +9,11 @@ function PageFooter() {
         <>
         <Stack display='flex' alignItems='center' justifyItems='center' width="100%" height='fit-content' paddingTop={5} position='relative'>
             <Stack display='flex' flexDir='row' alignContent='center' justifyContent='space-evenly' borderWidth={0.5} borderColor='rgb(197, 195, 195)' borderRadius={10} width='fit-content' position='absolute'>
-                <div className="headerTitle"><SiGmail /></div>
-                <div className="headerTitle"><FaGithub /></div>
-                <div className="headerTitle"><FaMediumM /></div>
-                <div className="headerTitle"><FaLinkedinIn /></div>
-                <div className="headerTitle"><FaTwitter /></div>
+                <a href="mailto:your-aryanhra58@gmail.com"><div className="headerTitle"><SiGmail /></div></a>
+                <a href="https://github.com/wolf1729"><div className="headerTitle"><FaGithub /></div></a>
+                <a href="https://medium.com/@aryanm1729"><div className="headerTitle"><FaMediumM /></div></a>
+                <a href="https://www.linkedin.com/in/aryan-mishra-78578b22a/"><div className="headerTitle"><FaLinkedinIn /></div></a>
+                <a href="https://x.com/wolf1729"><div className="headerTitle"><FaTwitter /></div></a>
             </Stack>
             <Particles quantity={500} color="black" className="absolute inset-0 headerComp"/>
         </Stack>
